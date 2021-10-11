72959348b7760a021d441909a1670fd58157fe2d
Mufaddal Hakim <mufaddalhakim15@gmail.com>
2021-10-11 15:39:22 +0530

    added feature to include words in random password

--- generator/views.py
+++ generator/views.py
@@ -11,6 +11,7 @@ def password(request):
 	alphabets = string.ascii_lowercase
 	length = int(request.GET.get('length', 13))
 	passcount = int(request.GET.get('passcount', 1))
+	include = request.GET.get('include', "")
 
 	if request.GET.get('uppercase'):
 		alphabets += string.ascii_uppercase
@@ -33,5 +34,12 @@ def password(request):
 		return finalpass
 
 	payload = [generate(alphabets,length) for i in range(passcount)]	
+	
+	for i in range(len(payload)):		
+		if len(include) != length:
+			start = random.randrange(0, length - len(include))		
+			payload[i] = payload[i][:start] + include + payload[i][start+len(include):]		
+		else:
+			payload[i] = include
 		
 	return render(request, 'generator/password.html', {'passwords': payload})
