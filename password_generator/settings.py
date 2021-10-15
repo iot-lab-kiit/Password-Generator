f129d5f75e267a07ac2cb078dd5f4b703edb1e10
bridyash13 <bridyash31@gmail.com>
2021-10-15 16:47:57 +0530

    Improved UI and added static folder to store static files

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Static Files path
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
